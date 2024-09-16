b64c7d53afc7c68cd914bbfa338e2b5c89efd715
lai2-24 <lai2-24@itismeucci.com>
2024-09-16 10:41:35 +0200

    tabella fatta

--- scipts/index.js
+++ scipts/index.js
@@ -5,18 +5,26 @@ creazioneTabella();
 function creazioneTabella(){
     const table = document.createElement('table');
     let num = 0;
+    let btnNum = 0
     table.classList.add('table');
-    for(let i = 0; i < 6; i++){
+    for(let i = 0; i < 7; i++){
         const row = document.createElement('tr');
         for(let j = 0; j < 7; j++){
             const element = document.createElement('td');
             element.classList.add('dimensioni');
             element.classList.add('bordi');
+            element.classList.add('casella');
             element.id = num;
-            element.addEventListener('click', (event) => {
-                gestisciSel(event.target.id);
-            });
-            num++;
+            if(i === 6){
+                element.style.backgroundColor = 'white'
+                element.id = btnNum;
+                element.addEventListener('click', (event) => {
+                    gestisciSel(event.target.id);
+                });
+                btnNum++;
+            }
+            else
+                num++;
             row.appendChild(element);
         }
         row.classList.add('dimensioni')
@@ -29,4 +37,13 @@ function creazioneTabella(){
 
 function gestisciSel(id){
     console.log('click' + id);
+    let caselle = document.querySelectorAll('casella')
+}
+
+function cambiaGiocatore(){
+    const body = document.querySelector('body');
+    if(body.style.backgroundColor === 'red')
+        body.style.backgroundColor = 'blue';
+    else
+        body.style.backgroundColor = 'red';
 }
